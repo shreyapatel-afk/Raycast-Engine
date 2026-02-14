84dae9e9042b2d79c674f7f6da142b3bcc3b4246
shreyapatel10 <patelshreya1244@gmail.com>
2026-02-14 22:10:53 +0530

    Delete quick-shader-main directory

--- quick-shader-main/qs/__init__.py
+++
@@ -1,6 +0,0 @@
-#  Final Class Structure for Quick Shader Library
-# ImageAnalyzer → Extracts shader-related data from an image.
-# ShaderModifier → Modifies shader attributes (brightness, contrast, light direction, etc.).
-# ShaderGenerator → Generates GLSL or HLSL shader files based on extracted & modified data.
-# ShaderPreview → Renders a preview of the shader for visualization.
-# ShaderUtils → Handles file operations, format conversions, and shader optimizations.
--- quick-shader-main/qs/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-os.path.exists(image_path)
